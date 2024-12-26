98ddca03a990fcf0415e0a83c65b3eded3dbd706
Isaac Vianna <60493567+isaacviannadev@users.noreply.github.com>
2024-12-26 23:29:53 +0000

    chore: fix build 3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,101 +2,33 @@ import { type Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{js,jsx,mdx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,mdx,ts,tsx}'],
   theme: {
-  	fontSize: {
-  		xs: [
-  			'0.75rem',
-  			{
-  				lineHeight: '1rem'
-  			}
-  		],
-  		sm: [
-  			'0.875rem',
-  			{
-  				lineHeight: '1.5rem'
-  			}
-  		],
-  		base: [
-  			'1rem',
-  			{
-  				lineHeight: '1.75rem'
-  			}
-  		],
-  		lg: [
-  			'1.125rem',
-  			{
-  				lineHeight: '1.75rem'
-  			}
-  		],
-  		xl: [
-  			'1.25rem',
-  			{
-  				lineHeight: '2rem'
-  			}
-  		],
-  		'2xl': [
-  			'1.5rem',
-  			{
-  				lineHeight: '2.25rem'
-  			}
-  		],
-  		'3xl': [
-  			'1.75rem',
-  			{
-  				lineHeight: '2.25rem'
-  			}
-  		],
-  		'4xl': [
-  			'2rem',
-  			{
-  				lineHeight: '2.5rem'
-  			}
-  		],
-  		'5xl': [
-  			'2.5rem',
-  			{
-  				lineHeight: '3rem'
-  			}
-  		],
-  		'6xl': [
-  			'3rem',
-  			{
-  				lineHeight: '3.5rem'
-  			}
-  		],
-  		'7xl': [
-  			'4rem',
-  			{
-  				lineHeight: '4.5rem'
-  			}
-  		]
-  	},
-  	extend: {
-  		borderRadius: {
-  			'4xl': '2.5rem',
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		fontFamily: {
-  			sans: [
-  				'Mona Sans',
-                    ...defaultTheme.fontFamily.sans
-                ],
-  			display: [
-  				[
-  					'Mona Sans',
-                        ...defaultTheme.fontFamily.sans
-                    ],
-  				{
-  					fontVariationSettings: 'wdth" 125'
-  				}
-  			]
-  		},
-  		colors: {}
-  	}
+    fontSize: {
+      xs: ['0.75rem', { lineHeight: '1rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+      '3xl': ['1.75rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['2.5rem', { lineHeight: '3rem' }],
+      '6xl': ['3rem', { lineHeight: '3.5rem' }],
+      '7xl': ['4rem', { lineHeight: '4.5rem' }],
+    },
+    extend: {
+      borderRadius: {
+        '4xl': '2.5rem',
+      },
+      fontFamily: {
+        sans: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+        display: [
+          ['Mona Sans', ...defaultTheme.fontFamily.sans],
+          { fontVariationSettings: '"wdth" 125' },
+        ],
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config
